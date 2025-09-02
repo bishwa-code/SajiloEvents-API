1a1c1eff34c933b816e105eb98d763ed2edf48d9
Bishwaranjan Kushwaha <bxk1415@gmail.com>
2025-09-03 04:07:34 +0545

    build

--- build/utils/generateToken.js
+++ build/utils/generateToken.js
@@ -20,7 +20,7 @@ const generateTokenAndSetCookie = (user, statusCode, res) => {
         .cookie("token", token, {
         httpOnly: true,
         secure: isProduction,
-        sameSite: isProduction ? "strict" : "lax",
+        sameSite: isProduction ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
         .json({

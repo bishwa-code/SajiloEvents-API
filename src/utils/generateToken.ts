2cf86dcb09e036c57f9ce27a3ec22b12ac018281
Bishwaranjan Kushwaha <bxk1415@gmail.com>
2025-09-03 03:44:51 +0545

    sameSite-none

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -31,7 +31,7 @@ const generateTokenAndSetCookie = (
     .cookie("token", token, {
       httpOnly: true,
       secure: isProduction,
-      sameSite: isProduction ? "strict" : "lax",
+      sameSite: isProduction ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     .json({

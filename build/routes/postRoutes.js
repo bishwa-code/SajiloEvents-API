b95185ae91f48c8d4b287b173df5e05f6fe005ad
Bishwaranjan Kushwaha <bxk1415@gmail.com>
2025-10-07 09:15:35 +0545

    build

--- build/routes/postRoutes.js
+++ build/routes/postRoutes.js
@@ -10,9 +10,9 @@ const uploadMiddleware_1 = require("../middlewares/uploadMiddleware"); // Import
 const router = express_1.default.Router();
 // Public routes
 router.get("/", postController_1.getAllPosts); // Get all posts
+router.get("/my", authMiddleware_1.protect, postController_1.getMyPosts); // Get posts created by the logged-in admin
 router.get("/:id", postController_1.getPostById); // Get single post by ID
 // Admin-only routes (protected and authorized)
-router.get("/posts", authMiddleware_1.protect, postController_1.getMyPosts); // Get posts created by the logged-in admin
 router.post("/", authMiddleware_1.protect, (0, authMiddleware_1.authorizeRoles)("admin"), (0, uploadMiddleware_1.uploadMultipleImages)("images"), postController_1.createPost); // Create post with multiple image upload
 router.put("/:id", authMiddleware_1.protect, (0, authMiddleware_1.authorizeRoles)("admin"), (0, uploadMiddleware_1.uploadMultipleImages)("images"), postController_1.updatePost); // Update post with optional multiple image upload
 router.delete("/:id", authMiddleware_1.protect, (0, authMiddleware_1.authorizeRoles)("admin"), postController_1.deletePost); // Delete post
